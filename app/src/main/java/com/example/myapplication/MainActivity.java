a3c48a5c4b6772cf53c743fce28ff5bc11f5033d
sunjiayuan123 <2772391183@qq.com>
2019-04-19 20:26:28 +0800

    //添加了注释

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private XRecyclerView mXre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        //孙家圆上传范德萨发生
 
+    }
+
+    private void initView() {
+        mXre = (XRecyclerView) findViewById(R.id.xre);
     }
 }
